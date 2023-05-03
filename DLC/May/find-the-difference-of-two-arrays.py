1e79201dc8bcf57d1208831b5ff51a3d7e778667
ayush0286 <127553281+ayush0286@users.noreply.github.com>
2023-05-03 21:56:55 +0530

    UB-72 [REWRITE] find-the-difference-of-two-arrays

--- DLC/May/find-the-difference-of-two-arrays.py
+++ DLC/May/find-the-difference-of-two-arrays.py
@@ -5,18 +5,5 @@
 class Solution:
   # O(n + m) Time | O(n + m) Space, where n and m are length of lists
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        set1 = {}
-        set2 = {}
-        for num in nums1:
-            set1[num] = True
-        for num in nums2:
-            set2[num] = True
-        
-        distincts = [[], []]
-        for num1 in set1:
-            if num1 not in set2:
-                distincts[0].append(num1)
-        for num2 in set2:
-            if num2 not in set1:
-                distincts[1].append(num2)
-        return distincts
+        set1, set2 = set(nums1), set(nums2)
+        return [list(set1 - set2), list(set2 - set1)]
